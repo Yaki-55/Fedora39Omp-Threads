58cf688874cc977dac304bb16c2be122702233cc
JesusSaid <vazj011224@gs.utm.mx>
2024-06-23 02:16:10 -0600

    update

--- BMP24bits_4hilos/main.cpp
+++ BMP24bits_4hilos/main.cpp
@@ -5,45 +5,45 @@
 #include <stdio.h>    
 #include "BMP.h"
 
-void *hilograyscale(void *param);
+void* hilograyscale(void* param);
 
-double timeval_diff(struct timeval *a, struct timeval *b)
+double timeval_diff(struct timeval* a, struct timeval* b)
 {
-    return (double)(a->tv_sec + (double)a->tv_usec/1000000) -
-           (double)(b->tv_sec + (double)b->tv_usec/1000000);
+    return (double)(a->tv_sec + (double)a->tv_usec / 1000000) -
+        (double)(b->tv_sec + (double)b->tv_usec / 1000000);
 }
 
 int main(int argc, char** argv) {
     struct timeval t_ini, t_fin;
     double secs;
-    BMP bmp ("nature.bmp");
-    
+    BMP bmp("nature.bmp");
+
     gettimeofday(&t_ini, NULL);
-    
+
     pthread_t threads[5]; // Array para almacenar los identificadores de los hilos
 
     // Creamos 4 hilos adicionales para procesamiento en paralelo
     for (int i = 0; i < 4; ++i) {
         pthread_create(&threads[i], NULL, hilograyscale, &bmp);
     }
-    
+
     // Esperamos a que todos los hilos terminen
     for (int i = 0; i < 4; ++i) {
         pthread_join(threads[i], NULL);
     }
-    
+
     gettimeofday(&t_fin, NULL);
-    
+
     secs = timeval_diff(&t_fin, &t_ini);
     printf("%.10g\n", secs * 1000.0);
-    
+
     bmp.save("grises.bmp");
-    
+
     return 0;
 }
 
-void *hilograyscale(void *param) {
-    BMP *bmp = (BMP *)param;
+void* hilograyscale(void* param) {
+    BMP* bmp = (BMP*)param;
     bmp->fromRGBtoGRAY();
     return NULL;
 }
--- BMP24bits_secuencial/main.cpp
+++ BMP24bits_secuencial/main.cpp
@@ -9,30 +9,30 @@ Autor: Christiam Mena
 #include <stdio.h>    
 #include "BMP.h"
 
-double timeval_diff(struct timeval *a, struct timeval *b)
+double timeval_diff(struct timeval* a, struct timeval* b)
 {
-	return	(double)(a->tv_sec + (double)a->tv_usec/1000000) -
-		(double)(b->tv_sec + (double)b->tv_usec/1000000);
+	return	(double)(a->tv_sec + (double)a->tv_usec / 1000000) -
+		(double)(b->tv_sec + (double)b->tv_usec / 1000000);
 }
 
 int main(int argc, char** argv)
 {
 	struct timeval t_ini, t_fin;
 	double secs;
-	BMP bmp ("nature.bmp");
-//	bmp.printHeader();
-//	bmp.blueChannel();
-	
+	BMP bmp("nature.bmp");
+	//	bmp.printHeader();
+	//	bmp.blueChannel();
+
 	gettimeofday(&t_ini, NULL);
-	
+
 	bmp.fromRGBtoGRAY();
-	
+
 	gettimeofday(&t_fin, NULL);
-	
+
 	secs = timeval_diff(&t_fin, &t_ini);
 	printf("%.10g\n", secs * 1000.0);
-	bmp.save("grises.bmp");	
+	bmp.save("grises.bmp");
 
-//    getchar();  // Con esta línea evitaremos que se cierre la consola
-    return 0;
+	//    getchar();  // Con esta línea evitaremos que se cierre la consola
+	return 0;
 }
